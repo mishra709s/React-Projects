84bd1257dd84c45f05b3d0f184d41a3159b374ac
Soubhagya Mishra <trippywords0101@gmail.com>
2019-07-06 20:24:09 +0530

    Updates

---
+++ precache-manifest.671f9e84ec27dc06109c6f19320efe65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac9126ef730b95e78d3136cd0a2685f",
+    "url": "/mishra709s/React-Projects/tree/master/contactmanager/index.html"
+  },
+  {
+    "revision": "1421daeb79fcf1d3fdc8",
+    "url": "/mishra709s/React-Projects/tree/master/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0a0b4c1c29fe4a58f6c3",
+    "url": "/mishra709s/React-Projects/tree/master/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1421daeb79fcf1d3fdc8",
+    "url": "/mishra709s/React-Projects/tree/master/contactmanager/static/js/2.30d665e9.chunk.js"
+  },
+  {
+    "revision": "0a0b4c1c29fe4a58f6c3",
+    "url": "/mishra709s/React-Projects/tree/master/contactmanager/static/js/main.c9673b4b.chunk.js"
+  },
+  {
+    "revision": "7c5b742a07a5546859d4",
+    "url": "/mishra709s/React-Projects/tree/master/contactmanager/static/js/runtime~main.c582a7d0.js"
+  }
+]);
